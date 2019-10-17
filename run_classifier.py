729ccbde50456ccb8806653a0f341995a50595c9
Diego Doe <diegovs87@yahoo.fr>
2019-10-17 22:14:42 +0200

    fixed bug with example tuples

--- run_classifier.py
+++ run_classifier.py
@@ -416,8 +416,8 @@ class AdeProcessor(DataProcessor):
     example_tuples = sorted(example_tuples, key=lambda t: t[0])
     for example_tuple in example_tuples:
         subset = self._get_random_subset()
-        self._examples[subset] = InputExample(guid=example[0], 
-            text_a=examples[1], text_b=None, label=example[2])
+        self._examples[subset] = InputExample(guid=example_tuples[0], 
+            text_a=example_tuples[1], text_b=None, label=example_tuples[2])
             
   def get_train_examples(self, data_dir):
     """See base class."""
